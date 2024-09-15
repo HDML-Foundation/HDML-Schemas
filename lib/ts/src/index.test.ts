37e44204d159267526a5a9ef76c2272ba38187b1
butarb <buntarb@gmail.com>
2024-09-15 13:08:53 +0000

    index.ts, export test

---
+++ lib/ts/src/index.test.ts
@@ -0,0 +1,55 @@
+/**
+ * @author Artem Lytvynov
+ * @copyright Artem Lytvynov
+ * @license Apache-2.0
+ */
+
+import * as schema from "./index";
+
+describe("@hdml/schema", () => {
+  it("must exports required types", () => {
+    expect(Object.keys(schema).length).toBe(42);
+    expect(schema.AggregationType).toBeDefined();
+    expect(schema.BigQueryParameters).toBeDefined();
+    expect(schema.CommonParameters).toBeDefined();
+    expect(schema.Connection).toBeDefined();
+    expect(schema.ConnectionOptions).toBeDefined();
+    expect(schema.ConnectionParameters).toBeDefined();
+    expect(schema.ConnectorTypes).toBeDefined();
+    expect(schema.DataType).toBeDefined();
+    expect(schema.DataTypeOptions).toBeDefined();
+    expect(schema.DateParameters).toBeDefined();
+    expect(schema.DateUnit).toBeDefined();
+    expect(schema.DecimalBitWidth).toBeDefined();
+    expect(schema.DecimalParameters).toBeDefined();
+    expect(schema.Document).toBeDefined();
+    expect(schema.ElasticsearchParameters).toBeDefined();
+    expect(schema.ExpressionParameters).toBeDefined();
+    expect(schema.Field).toBeDefined();
+    expect(schema.FieldType).toBeDefined();
+    expect(schema.Filter).toBeDefined();
+    expect(schema.FilterClause).toBeDefined();
+    expect(schema.FilterName).toBeDefined();
+    expect(schema.FilterOperator).toBeDefined();
+    expect(schema.FilterOptions).toBeDefined();
+    expect(schema.FilterType).toBeDefined();
+    expect(schema.Frame).toBeDefined();
+    expect(schema.GoogleSheetsParameters).toBeDefined();
+    expect(schema.Include).toBeDefined();
+    expect(schema.JDBCParameters).toBeDefined();
+    expect(schema.Join).toBeDefined();
+    expect(schema.JoinType).toBeDefined();
+    expect(schema.KeysParameters).toBeDefined();
+    expect(schema.Model).toBeDefined();
+    expect(schema.MongoDBParameters).toBeDefined();
+    expect(schema.NamedParameters).toBeDefined();
+    expect(schema.OrderType).toBeDefined();
+    expect(schema.SnowflakeParameters).toBeDefined();
+    expect(schema.Table).toBeDefined();
+    expect(schema.TableType).toBeDefined();
+    expect(schema.TimeParameters).toBeDefined();
+    expect(schema.TimeUnit).toBeDefined();
+    expect(schema.TimeZone).toBeDefined();
+    expect(schema.TimestampParameters).toBeDefined();
+  });
+});
--- lib/ts/src/index.ts
+++ lib/ts/src/index.ts
@@ -1,3 +1,53 @@
-import * as enums from "./enum";
+/**
+ * @author Artem Lytvynov
+ * @copyright Artem Lytvynov
+ * @license Apache-2.0
+ */
 
-export enums;
+/* eslint-disable max-len */
+
+// Enums
+export { AggregationType } from "./enum/aggregation-type";
+export { ConnectorTypes } from "./enum/connector-types";
+export { DataType } from "./enum/data-type";
+export { DateUnit } from "./enum/date-unit";
+export { DecimalBitWidth } from "./enum/decimal-bit-width";
+export { FilterName } from "./enum/filter-name";
+export { FilterOperator } from "./enum/filter-operator";
+export { FilterType } from "./enum/filter-type";
+export { JoinType } from "./enum/join-type";
+export { OrderType } from "./enum/order-type";
+export { TableType } from "./enum/table-type";
+export { TimeUnit } from "./enum/time-unit";
+export { TimeZone } from "./enum/time-zone";
+
+// Documents
+export { BigQueryParameters } from "./document/big-query-parameters";
+export { CommonParameters } from "./document/common-parameters";
+export { Connection } from "./document/connection";
+export { ConnectionOptions } from "./document/connection-options";
+export { ConnectionParameters } from "./document/connection-parameters";
+export { DataTypeOptions } from "./document/data-type-options";
+export { DateParameters } from "./document/date-parameters";
+export { DecimalParameters } from "./document/decimal-parameters";
+export { Document } from "./document/document";
+export { ElasticsearchParameters } from "./document/elasticsearch-parameters";
+export { ExpressionParameters } from "./document/expression-parameters";
+export { Field } from "./document/field";
+export { FieldType } from "./document/field-type";
+export { Filter } from "./document/filter";
+export { FilterClause } from "./document/filter-clause";
+export { FilterOptions } from "./document/filter-options";
+export { Frame } from "./document/frame";
+export { GoogleSheetsParameters } from "./document/google-sheets-parameters";
+export { Include } from "./document/include";
+export { JDBCParameters } from "./document/jdbcparameters";
+export { Join } from "./document/join";
+export { KeysParameters } from "./document/keys-parameters";
+export { Model } from "./document/model";
+export { MongoDBParameters } from "./document/mongo-dbparameters";
+export { NamedParameters } from "./document/named-parameters";
+export { SnowflakeParameters } from "./document/snowflake-parameters";
+export { Table } from "./document/table";
+export { TimeParameters } from "./document/time-parameters";
+export { TimestampParameters } from "./document/timestamp-parameters";
